9f4d3619aaba81edeca0d8a19821b7948348a630
ZARIFMAC <uzbzarif@gmail.com>
2022-12-22 15:38:00 +0500

    Create todo

--- todoAPI/views.py
+++ todoAPI/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request 
+from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Todo
+
+from .serializers import TodoSerializer
+
+# Create todo task
+
+class CreateTodoView(APIView):
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
